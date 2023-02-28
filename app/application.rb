824f522f676f6acdd401513b93e4e6c1997b6cd1
Teresa <tenjerim@gmail.com>
2023-02-28 20:19:45 +0300

    completed

--- app/application.rb
+++ app/application.rb
@@ -160,4 +160,18 @@ class Application
     end
   
   end
-  
+
+  # GET /projects - get all project
+# PUT /projects - create new project
+# PATCH /project/:id - update project name
+# DELETE /project/:id - delete project (maybe also delete all boards and tasks associated)
+
+# GET /boards - get all boards associated to project
+# PUT /boards - create new board within project
+# PATCH /boards/:id - update board name
+# DELETE /boards/:id - delete board (maybe also delete all tasks associated)
+
+# GET /tasks - get all tasks associated with board
+# PUT /tasks - create new task within board
+# PATCH /tasks/:id - update task
+# DELETE /tasks/:id - delete task
