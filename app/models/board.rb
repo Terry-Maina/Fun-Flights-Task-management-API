b5748e7f7650865103fb7469597eaadc8b892d0e
Teresa <tenjerim@gmail.com>
2023-02-28 11:55:46 +0300

    completed

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,3 +2,6 @@ require_relative './models_module'
 
 class Board < ActiveRecord::Base
     extend InheritMethods::ClassMethods #extend is for class methods
+
+    belongs_to :project 
+    has_many :tasks, dependent: :destroy
