046b144a80d6e56a744f6086892865231b4fd815
Teresa <tenjerim@gmail.com>
2023-02-28 12:10:54 +0300

    completed

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,4 +8,22 @@ class Task < ActiveRecord::Base
 
     def self.render_all_formatted_for_frontend
         self.all.map do |task|
-            
+            {
+            id: task.id,
+            name: task.name,
+            due_date: task.due_date,
+            description: task.description,
+            status: task.status,
+            priority: task.priority,
+            completed: task.completed,
+            board: task.board.name,
+            board_id: task.board_id,
+            project: task.board.project.title,
+            project_id: task.board.project_id,
+            created_at: task.created_at,
+            updated_at: task.updated_at
+            }
+        end
+    end
+
+    
