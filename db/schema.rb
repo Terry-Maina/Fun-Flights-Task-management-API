c60202da26d80dd649d6e344a06d06d1d217dc4f
Teresa <tenjerim@gmail.com>
2023-03-03 03:05:27 +0300

    readme

--- db/schema.rb
+++ db/schema.rb
@@ -2,43 +2,43 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_28_215741) do
+ActiveRecord::Schema.define(version: 2023_03_02_152544) do
 
-    create_table "boards", force: :cascade do |t|
-      t.string "name"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.integer "project_id"
-      t.index ["project_id"], name: "index_boards_on_project_id"
-    end
-  
-    create_table "projects", force: :cascade do |t|
-      t.string "title"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.boolean "favorite"
-      t.string "color"
-    end
-  
-    create_table "tasks", force: :cascade do |t|
-      t.string "name"
-      t.date "due_date"
-      t.text "description"
-      t.string "status"
-      t.string "priority"
-      t.boolean "completed"
-      t.integer "board_id"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.index ["board_id"], name: "index_tasks_on_board_id"
-    end
-  
+  create_table "boards", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "project_id"
+    t.index ["project_id"], name: "index_boards_on_project_id"
   end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "favorite"
+    t.string "color"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.date "due_date"
+    t.text "description"
+    t.string "status"
+    t.string "priority"
+    t.boolean "completed"
+    t.integer "board_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["board_id"], name: "index_tasks_on_board_id"
+  end
+
+end
